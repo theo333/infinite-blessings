93983f59dd5e25232c2a4ab073b2fab2e5b6666a
Theo Manton <theo333@gmail.com>
2019-09-08 20:57:11 -0400

    add server

---
+++ server.js
@@ -0,0 +1,10 @@
+const db = require('./db');
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+db.syncAndSeed()
+  .then(() => {
+    app.listen(port, () => console.log(`listening on port ${port}`));
+  })
+
