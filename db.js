137db7fd317f65980b5aa27068bd72087772fb2b
Theo Manton <theo333@gmail.com>
2019-09-08 23:10:59 -0400

    add Question model, seed questions and blessings models

--- db.js
+++ db.js
@@ -9,15 +9,13 @@ const Blessing = conn.define('blessing', {
     defaultValue: UUIDV4,
     primaryKey: true
   },
-  firstName: {
+  name: {
     type: STRING,
     allowNull: false,
     validate: {
-      notEmpty: true
+      notEmpty: true,
+      // char limit 15 char
     }
-  },
-  lastName: {
-    type: STRING
   },
   comment: {
     type: STRING
@@ -27,19 +25,57 @@ const Blessing = conn.define('blessing', {
   }
 });
 
+const Stats = conn.define('stats', {
+  id: {
+    type: UUID,
+    primaryKey: true
+  },
+  blessingsTotal: {
+    type: INTEGER,
+  },
+  blessingsQty: {
+    type: INTEGER
+  }
+});
+
+const Question = conn.define('question', {
+  id: {
+    type: UUID,
+    defaultValue: UUIDV4,
+    primaryKey: true
+  },
+  name: {
+    type: STRING
+  }
+});
+
+const mapSeed = (data, model) => Promise.all(data.map(item => model.create(item)));
+
 const syncAndSeed = async () => {
   await conn.sync({ force: true })
 
   const blessings = [
-    { firstName: 'Joe' },
-    { firstName: 'Susan' },
-    { firstName: 'Lisa' },
-    { firstName: 'Charlie' }
+    { name: 'Joe', blessingsNum: 5 },
+    { name: 'Susan', blessingsNum: 20 },
+    { name: 'Lisa', blessingsNum: 50 },
+    { name: 'Charlie', blessingsNum: 100 }
   ];
 
-  const [joe, susan, lisa, charlie] = await Promise.all(blessings.map(blessing => Blessing.create(blessing)));
+  // const [joe, susan, lisa, charlie] = await Promise.all(blessings.map(blessing => Blessing.create(blessing)));
+  const [joe, susan, lisa, charlie] = await mapSeed(blessings, Blessing);
 
-  console.log('joe', joe.get());
+  console.log('joe', susan.get());
+
+  const questions = [
+    { name: 'Question 1' },
+    { name: 'Question 2' },
+    { name: 'Question 3' },
+    { name: 'Question 4' },
+  ];
+
+  const [question1, question2, question3, question4] = await mapSeed(questions, Question);
+
+  console.log('question1', question1.get());
 
   return {
     blessings: {
@@ -47,14 +83,21 @@ const syncAndSeed = async () => {
       susan,
       lisa,
       charlie
+    },
+    questions: {
+      question1,
+      question2,
+      question3,
+      question4
     }
-  }
+  };
 };
 
 module.exports = {
   syncAndSeed,
   models: {
-    Blessing
+    Blessing,
+    Question
   }
 };
 
